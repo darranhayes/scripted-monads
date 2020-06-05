9a52a04053aeca5e6ccc26906ce4d71276e04eac
Darran Hayes <2190584+darranhayes@users.noreply.github.com>
2020-06-05 11:52:04 +0100

    Add traditional composition call style

--- Maybe.fsx
+++ Maybe.fsx
@@ -1,8 +1,9 @@
+open System
+
 type Maybe<'a> =
 | Something of 'a
 | Nothing
 
-[<RequireQualifiedAccess>]
 module Maybe =
     let map f m =
         match m with
@@ -14,14 +15,23 @@ module Maybe =
         | Something x -> f x
         | Nothing -> Nothing
 
-    type MaybeBuilder() =
-        member x.Bind(comp, func) = bind func comp
-        member x.Return(value) = Something value
+let print x =
+    match x with
+    | Something x -> printfn "Something: %O" x
+    | Nothing -> printfn "Nothing"
 
-let maybe = Maybe.MaybeBuilder()
+(* Traditional long handed composition *)
+let a = Something 3
+let b = Something 4
+let c = Something 10
 
-open System
+Maybe.bind (fun x -> Maybe.map (fun y -> x + y) b) a
+|> print
 
+Maybe.bind (fun x -> Maybe.map (fun y -> Maybe.map (fun z -> x + y + z) c) b) a
+|> print
+
+(* Pipeline style *)
 let tryParseInt (x : string) =
     let result = Int32.TryParse(x)
     match result with
@@ -33,12 +43,8 @@ let isOld age =
     | x when x > 40 -> true
     | x -> false
 
-let print x =
-    match x with
-    | Something x -> printfn "Something: %O" x
-    | Nothing -> printfn "Nothing"
-
-type CustomerRepository = int -> Maybe<{| Id : int; Age : Maybe<int> |}>
+type Customer = { Id : int; Age : int Maybe }
+type CustomerRepository = int -> Customer Maybe
 
 let showCustomerAge (customerRepo: CustomerRepository) strCustomerId =
     tryParseInt strCustomerId
@@ -46,21 +52,17 @@ let showCustomerAge (customerRepo: CustomerRepository) strCustomerId =
     |> Maybe.bind (fun cust -> cust.Age)
     |> Maybe.map isOld
 
-showCustomerAge (fun id -> Something {| Id = id; Age = Something id |}) "42" |> print
-showCustomerAge (fun id -> Something {| Id = id; Age = Something id |}) "35" |> print
-showCustomerAge (fun id -> Something {| Id = id; Age = Nothing |}) "42" |> print
+showCustomerAge (fun id -> Something { Id = id; Age = Something id }) "42" |> print
+showCustomerAge (fun id -> Something { Id = id; Age = Something id }) "35" |> print
+showCustomerAge (fun id -> Something { Id = id; Age = Nothing }) "42" |> print
 showCustomerAge (fun _ -> Nothing) "42" |> print
 
-// Something 5 + Something 3
-Something 5
-|> Maybe.bind (fun x -> Something 3 |> Maybe.map ((+) x))
-|> print
+(* Computation expressions *)
+type MaybeBuilder() =
+    member __.Bind(m, func) = Maybe.bind func m
+    member __.Return(x) = Something x
 
-// Something 5 + Nothing + Something 5
-Something 5
-|> Maybe.bind (fun x -> Nothing |> Maybe.map ((+) x))
-|> Maybe.bind (fun x -> Something 5 |> Maybe.map ((+) x))
-|> print
+let maybe = MaybeBuilder()
 
 maybe {
     let! a = Something 5
