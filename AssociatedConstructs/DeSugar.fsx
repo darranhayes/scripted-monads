aa9c9505b3b8219bff8afd7ab882d945456df742
Darran Hayes <2190584+darranhayes@users.noreply.github.com>
2020-06-10 12:57:54 +0100

    Add example of de-sugaring technique used in State.fsx

---
+++ AssociatedConstructs/DeSugar.fsx
@@ -0,0 +1,13 @@
+﻿(* DeSugaring a function with support for generic arguments *)
+
+type Delegate<'a, 'b> = Delegate of ('a -> 'b * 'a)
+
+let deSugar delegateInstance arg = // this function returns f(arg)
+    let (Delegate(f)) = delegateInstance in f arg
+
+let d = Delegate(fun x -> x.ToString(), x)
+
+let func = deSugar d
+let value = func 5
+
+printfn "%s" (value |> fst)
