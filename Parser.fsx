ff2b7a93c05c1c4af9e9fb3a768104f27a5425b6
Darran Hayes <2190584+darranhayes@users.noreply.github.com>
2023-04-13 11:12:01 +0100

    Parse any char

--- Parser.fsx
+++ Parser.fsx
@@ -1,16 +1,20 @@
 // https://fsharpforfunandprofit.com/posts/understanding-parser-combinators/
 
-type Parser = string -> bool * string
+type Parser = char * string -> string * string
 
-let parseA : Parser = fun str ->
-    let c = str[0]
-    let remainder = str[1..]
+let pchar : Parser = fun (charToMatch, str) ->
     if str = "" then
-        (false, "")
+        ("No more input", "")
     else
-        if c = 'A' then
-            (true, remainder)
+        let first = str[0]
+        if first = charToMatch then
+            let remainding = str[1..]
+            let msg = $"Found {first}"
+            (msg, remainding)
         else
-            (false, str)
+            let msg = $"Expecting: {charToMatch}, but got: {first}"
+            (msg, str)
 
-parseA ""
+pchar ('A', "")
+pchar ('A', "ZBC")
+pchar ('A', "ABC")
