e45d2c1ea3e07034f88901c195475cc67ec4c543
Darran Hayes <2190584+darranhayes@users.noreply.github.com>
2023-04-13 12:17:44 +0100

    Add andThen combinator

--- Parser.fsx
+++ Parser.fsx
@@ -4,16 +4,16 @@ type ParseResult<'a> =
     | Success of 'a
     | Failure of string
 
-type Parser<'a> = Parser of (string -> ParseResult<'a>)
+type Parser<'a> = Parser of (string -> ParseResult<'a * string>)
 
 let pchar charToMatch =
-    let innerFn str =
-        if str = "" then
+    let innerFn input =
+        if input = "" then
             Failure "No more input"
         else
-            let first = str[0]
+            let first = input[0]
             if first = charToMatch then
-                let remainding = str[1..]
+                let remainding = input[1..]
                 let msg = $"Found {first}"
                 Success (charToMatch, remainding)
             else
@@ -25,6 +25,30 @@ let run parser input =
     let (Parser innerFn) = parser
     innerFn input
 
-let parseA = pchar 'A'
+let andThen (parser1: Parser<'a>) (parser2: Parser<'b>) : Parser<'a * 'b> =
+    let innerFn input =
+        match run parser1 input with
+        | Failure err ->
+            Failure err
+        | Success (value1, remaining1) ->
+            match run parser2 remaining1 with
+            | Failure err ->
+                Failure err
+            | Success (value2, remaining2) ->
+                let newValue = value1, value2
+                Success (newValue, remaining2)
+    Parser innerFn
 
-run parseA "ABC"
+let (.>>.) = andThen
+
+let parseA: Parser<char> = pchar 'A'
+let parseB: Parser<char> = pchar 'B'
+
+let parseAThenB = parseA .>>. parseB
+
+run parseAThenB ""
+run parseAThenB "A"
+run parseAThenB "AB"
+run parseAThenB "AC"
+run parseAThenB "ABC"
+run parseAThenB "ZBC"
