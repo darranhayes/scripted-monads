3dd2608b97a53a14916d78a453c8724076ef0ee5
Darran Hayes <2190584+darranhayes@users.noreply.github.com>
2023-04-13 11:47:09 +0100

    Curried pchar

--- Parser.fsx
+++ Parser.fsx
@@ -1,20 +1,26 @@
 // https://fsharpforfunandprofit.com/posts/understanding-parser-combinators/
 
-type Parser = char * string -> string * string
+type ParseResult<'a> =
+    | Success of 'a
+    | Failure of string
 
-let pchar : Parser = fun (charToMatch, str) ->
-    if str = "" then
-        ("No more input", "")
-    else
-        let first = str[0]
-        if first = charToMatch then
-            let remainding = str[1..]
-            let msg = $"Found {first}"
-            (msg, remainding)
+type Parser<'a> = 'a -> (string -> ParseResult<'a>)
+
+let pchar charToMatch =
+    let innerFn str =
+        if str = "" then
+            Failure "No more input"
         else
-            let msg = $"Expecting: {charToMatch}, but got: {first}"
-            (msg, str)
+            let first = str[0]
+            if first = charToMatch then
+                let remainding = str[1..]
+                let msg = $"Found {first}"
+                Success (charToMatch, remainding)
+            else
+                let msg = $"Expecting: {charToMatch}, but got: {first}"
+                Failure msg
+    innerFn
 
-pchar ('A', "")
-pchar ('A', "ZBC")
-pchar ('A', "ABC")
+pchar 'A' ""
+pchar 'A' "ZBC"
+pchar 'A' "ABC"
