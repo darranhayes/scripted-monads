29b67abbee56d23fa55c50e72a4a0af52560543e
Darran Hayes <2190584+darranhayes@users.noreply.github.com>
2023-04-16 10:01:28 +0100

    Reorganise and tidy

--- Parser.fsx
+++ Parser.fsx
@@ -14,12 +14,55 @@ module Parser =
     type Parser<'a> = {
         ParserFn : string -> ParseResult<'a * string>
         Label : ParserLabel
-    }
+    } with
+        static member create
+            (fn: string -> ParseResult<'a * string>)
+            (label: ParserLabel)
+            : Parser<'a> =
+            { ParserFn = fn; Label = label }
+
+    let run (parser: Parser<'a>) (input: string) : ParseResult<'a * string> =
+        parser.ParserFn input
 
     let charListToString chars =
         chars
         |> List.toArray
         |> System.String
+
+    let charListToInt list =
+        list
+        |> List.toArray
+        |> System.String
+        |> int
+
+    let rec parseZeroOrMore (p: Parser<'a>) (input: string) : list<'a> * string =
+        match run p input with
+        | Failure _ ->
+            ([], input)
+        | Success (value1, remaining1) ->
+            let (value2, remaining2 ) =
+                parseZeroOrMore p remaining1
+            (value1::value2, remaining2)
+
+    let satisfy predicate (label: string) =
+        let innerFn input =
+            if System.String.IsNullOrEmpty input then
+                Failure (label, "No more input")
+            else
+                let first = input[0]
+                if predicate first then
+                    let remaining = input[1..]
+                    Success (first, remaining)
+                else
+                    let error = sprintf "Unexpected: '%c'" first
+                    Failure (label, error)
+
+        Parser.create innerFn label
+
+    let pChar charToMatch =
+        let predicate ch = (ch = charToMatch)
+        let label = sprintf "'%c'" charToMatch
+        satisfy predicate label
 
     let printResult (result: ParseResult<'a * string>) =
         match result with
@@ -35,22 +78,19 @@ module Parser =
                 Failure (newLabel, error)
             | Success s ->
                 Success s
-        { ParserFn = innerFn; Label = newLabel }
+        Parser.create innerFn newLabel
 
     let (<?>) = setLabel
 
     let getLabel (p: Parser<'a>) : string =
         p.Label
 
-    let run (parser: Parser<'a>) (input: string) : ParseResult<'a * string> =
-        parser.ParserFn input
-
     /// returnP lifts a normal value into the world of parsers.
     let returnP (x: 'a) : Parser<'a> =
         let label = sprintf "%A" x
         let innerFn input =
             Success (x, input)
-        { ParserFn = innerFn; Label = label }
+        Parser.create innerFn label
 
     /// bindP chains the result of a parser to another parser-producing function.
     let bindP (f: 'a -> Parser<'b>) (p: Parser<'a>) : Parser<'b> =
@@ -62,7 +102,7 @@ module Parser =
             | Success (value1, remainder1) ->
                 let newParser = f value1
                 run newParser remainder1
-        { ParserFn = innerFn; Label = label }
+        Parser.create innerFn label
 
     /// bindP
     let (>>=) (p: Parser<'a>) (f: 'a -> Parser<'b>) : Parser<'b> =
@@ -76,18 +116,6 @@ module Parser =
     let (<!>) =
         mapP
 
-    /// andThen compose two parsers
-    let andThen (p1: Parser<'a>) (p2: Parser<'b>) : Parser<'a * 'b> =
-        let label = sprintf "'%s' andThen '%s'" (getLabel p1) (getLabel p2)
-        p1 >>= (fun p1Result ->
-        p2 >>= (fun p2Result ->
-            returnP (p1Result, p2Result)))
-        <?> label
-
-    /// andThen
-    let (.>>.) =
-        andThen
-
     /// applyP lifts multi-parameter functions into functions that work on parsers.
     let applyP (fP: Parser<'a -> 'b>) (xP: Parser<'a>) : Parser<'b> =
         fP >>= (fun f ->
@@ -98,8 +126,24 @@ module Parser =
     let (<*>) =
         applyP
 
+    /// lift binary functions and apply to parsers
+    let lift2 (f: 'a -> 'b -> 'c) (xP: Parser<'a>) (yP: Parser<'b>) : Parser<'c> =
+        returnP f <*> xP <*> yP
+
+    /// andThen compose two parsers
+    let andThen (p1: Parser<'a>) (p2: Parser<'b>) : Parser<'a * 'b> =
+        let label = sprintf "%s andThen %s" (getLabel p1) (getLabel p2)
+        p1 >>= (fun p1Result ->
+        p2 >>= (fun p2Result ->
+            returnP (p1Result, p2Result)))
+        <?> label
+
+    /// andThen
+    let (.>>.) =
+        andThen
+
     let orElse (p1: Parser<'a>) (p2: Parser<'a>) : Parser<'a> =
-        let label = sprintf "either '%s' orElse '%s'" (getLabel p1) (getLabel p2)
+        let label = sprintf "%s orElse %s" (getLabel p1) (getLabel p2)
 
         let innerFn input =
             match run p1 input with
@@ -108,7 +152,7 @@ module Parser =
                 | Failure _ ->
                     run p2 input
 
-        { ParserFn = innerFn; Label = label }
+        Parser.create innerFn label
         <?> label
 
     /// orElse
@@ -132,14 +176,9 @@ module Parser =
     let choice (list: Parser<'a> list) : Parser<'a> =
         List.reduce (<|>) list
 
-    /// lift binary functions and apply to parsers
-    let lift2 (f: 'a -> 'b -> 'c) (xP: Parser<'a>) (yP: Parser<'b>) : Parser<'c> =
-        returnP f <*> xP <*> yP
-
     /// sequence converts a list of parsers into a parser containing a list.
     let rec sequence (list: Parser<'a> list) : Parser<'a list> =
         let cons head tail = head::tail
-
         let consP = lift2 cons
 
         match list with
@@ -148,83 +187,59 @@ module Parser =
         | x::xs ->
             consP x (sequence xs)
 
-    let rec parseZeroOrMore (p: Parser<'a>) (input: string) : list<'a> * string =
-        match run p input with
-        | Failure _ ->
-            ([], input)
-        | Success (value1, remaining1) ->
-            let (value2, remaining2 ) =
-                parseZeroOrMore p remaining1
-            (value1::value2, remaining2)
-
     /// many matches zero or more occurrences of the specified parser.
     let many (p: Parser<'a>) : Parser<'a list> =
-        let label = sprintf "many %s" (p.Label)
+        let label = sprintf "many %s" (getLabel p)
         let innerFn input =
             Success (parseZeroOrMore p input)
-
-        { ParserFn = innerFn; Label = label }
+        Parser.create innerFn label
 
     /// many1 matches one or more occurrences of the specified parser.
     let many1 (p: Parser<'a>) : Parser<'a list> =
+        let label = sprintf "many %s" (getLabel p)
         p >>= (fun head ->
         many p >>= (fun tail ->
             returnP (head::tail)))
-
-    let satisfy predicate (label: string) =
-        let innerFn input =
-            if System.String.IsNullOrEmpty input then
-                Failure (label, "No more input")
-            else
-                let first = input[0]
-                if predicate first then
-                    let remaining = input[1..]
-                    Success (first, remaining)
-                else
-                    let error = sprintf "Unexpected: '%c'" first
-                    Failure (label, error)
-
-        { ParserFn = innerFn; Label = label }
-
-    let pchar charToMatch =
-        let predicate ch = (ch = charToMatch)
-        let label = sprintf "'%c'" charToMatch
-        satisfy predicate label
+        <?> label
 
     let anyOf listOfChars =
         let label = sprintf "anyOf %A" listOfChars
         listOfChars
-        |> List.map pchar
+        |> List.map pChar
         |> choice
         <?> label
 
     /// opt matches an optional occurrence of the specified parser.
-    let opt (p : Parser<'a>) : Parser<'a option> =
+    let optional (p : Parser<'a>) : Parser<'a option> =
         let some = p |>> Some
         let none = returnP None
         some <|> none
+        <?> sprintf "optional %s" (getLabel p)
 
-    /// between keeps only the result of the middle parser.
-    let between (p1: Parser<'a>) (p2: Parser<'b>) (p3: Parser<'c>) : Parser<'b> =
+    /// betweenInclusive returns the middle parser surrounded by the boundary parsers.
+    let betweenInclusive (p1: Parser<'a>) (p2: Parser<'b>) (p3: Parser<'c>) : Parser<('a * 'b) * 'c> =
+        (p1 .>>. p2) .>>. p3
+        <?> sprintf "%s%s%s" (getLabel p1) (getLabel p2) (getLabel p3)
+
+    /// betweenExclusive keeps only the result of the middle parser.
+    let betweenExclusive (p1: Parser<'a>) (p2: Parser<'b>) (p3: Parser<'c>) : Parser<'b> =
         p1 >>. p2 .>> p3
-        <?> sprintf "%s %s %s" (p1.Label) (p2.Label) (p3.Label)
+        <?> sprintf "%s%s%s" (getLabel p1) (getLabel p2) (getLabel p3)
 
-    /// sepBy1 parses one or more occurrences of a parser with a separator.
-    let sepBy1 (p: Parser<'a>) (separator: Parser<'b>) : Parser<'a list> =
+    /// separateBy1 parses one or more occurrences of a parser with a separator.
+    let separateBy1 (p: Parser<'a>) (separator: Parser<'b>) : Parser<'a list> =
         p .>>. many (separator >>. p)
         |>> List.Cons
+        <?> sprintf "list of %s separated by %s" (getLabel p) (getLabel p)
 
-    /// sepBy parses zero or more occurrences of a parser with a separator.
-    let sepBy (p: Parser<'a>) (separator: Parser<'b>) : Parser<'a list> =
-        sepBy1 p separator <|> returnP []
+    /// separateBy parses zero or more occurrences of a parser with a separator.
+    let separateBy (p: Parser<'a>) (separator: Parser<'b>) : Parser<'a list> =
+        separateBy1 p separator <|> returnP []
 
     let whitespaceChar =
         let predicate = System.Char.IsWhiteSpace
         let label = "whitespace"
         satisfy predicate label
-
-    let whitespace =
-        many whitespaceChar
 
     let spaces =
         many whitespaceChar
@@ -235,21 +250,16 @@ module Parser =
     let punctuationChar =
         let predicate = System.Char.IsPunctuation
         let label = "punctuation"
-
         satisfy predicate label
 
     let pString (input: string) : Parser<string> =
+        let label = sprintf "string %s" input
         input
         |> Seq.toList
-        |> List.map pchar
+        |> List.map pChar
         |> sequence
         |>> charListToString
-
-    let charListToInt list =
-        list
-        |> List.toArray
-        |> System.String
-        |> int
+        <?> label
 
     let pDigit =
         let predicate = System.Char.IsDigit
@@ -260,6 +270,7 @@ module Parser =
         anyOf [ '-'; '+' ]
 
     let pInt: Parser<int> =
+        let label = "signed int"
         let resultToInt (sign, chars) =
             let i =
                 chars
@@ -270,64 +281,19 @@ module Parser =
             | _ -> i
 
         let sign =
-            opt pSign
+            optional pSign
         let digits =
             many1 pDigit
 
         sign .>>. digits
         |>> resultToInt
+        <?> label
 
 (* *)
 open Parser
 
-let parseOp =
-    whitespace >>. anyOf [ '*'; '/'; '+'; '-' ] .>> whitespace
+let oi = pInt
+let c = pChar 'A'
+let accessModifier = pString "public" <|> pString "private"
 
-let parseLParen =
-    whitespace .>>. pchar '(' .>>. whitespace
-    <?> "("
-
-let parseRParen =
-    whitespace .>>. pchar ')' .>>. whitespace
-    <?> ")"
-
-let parseTerm =
-    whitespace >>. pInt .>> whitespace
-    <?> "int"
-
-let expr =
-    parseTerm .>>. parseOp .>>. parseTerm
-    <?> "term op term"
-
-let expression =
-    let withParens =
-        between parseLParen expr parseRParen
-    expr <|> withParens
-
-run expression " ( +123  +  -456 ) "
-run expression " -123 / +1 "
-
-let comma = pchar ','
-let digit = anyOf ['0'..'9'] <?> "digit"
-
-run digit "fds"
-
-let zeroOrMoreDigitList =
-    sepBy digit comma
-    <?> "list of digits separated by ','"
-
-let oneOrMoreDigitList =
-    sepBy1 digit comma
-    <?> "list of digits separated by ','"
-
-run oneOrMoreDigitList "1;"      // Success (['1'], ";")
-run oneOrMoreDigitList "1,2;"    // Success (['1'; '2'], ";")
-run oneOrMoreDigitList "1,2,3;"  // Success (['1'; '2'; '3'], ";")
-run oneOrMoreDigitList "Z;"      // Failure "Expecting '9'. Got 'Z'"
-|> printResult
-
-run zeroOrMoreDigitList "1;"     // Success (['1'], ";")
-run zeroOrMoreDigitList "1,2;"   // Success (['1'; '2'], ";")
-run zeroOrMoreDigitList "1,2,3;" // Success (['1'; '2'; '3'], ";")
-run zeroOrMoreDigitList "Z;"     // Success ([], "Z;")
-
+run c "a3" |> printResult
