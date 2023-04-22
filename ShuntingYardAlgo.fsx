d0129b44fa2d606c1070da1bb2c40fe2c3bc8ea7
Darran Hayes <2190584+darranhayes@users.noreply.github.com>
2023-04-22 09:59:02 +0100

    Shunting Yard Algorithm with 1 stack

--- ShuntingYardAlgo.fsx
+++ ShuntingYardAlgo.fsx
@@ -21,55 +21,62 @@ let (|Operator|Term|) (t: string) =
     | "-" -> Operator t
     | n -> Term n
 
-let rebuildTerm (terms, operators) =
-    let arg2, terms =
-        terms |> Stack.pop
-    let arg1, terms =
-        terms |> Stack.pop
+let rebuildTerm (parseStack: Stack<string>) : Stack<string> =
+    let arg2, parseStack =
+        parseStack |> Stack.pop
 
-    let operator, operators =
-        operators |> Stack.pop
+    let operator, parseStack =
+        parseStack |> Stack.pop
+
+    let arg1, parseStack =
+        parseStack |> Stack.pop
 
     let newExpr =
         sprintf "(%s %s %s)" arg1 operator arg2
 
-    Stack.push newExpr terms, operators
+    Stack.push newExpr parseStack
 
-let rec handleOperator currentOperator (terms, operators) =
+let rec handleOperator (currentOperator: string) (parseStack: Stack<string>) : Stack<string> =
     let previousOperator =
-        Stack.tryPeak 0 operators
+        Stack.tryPeak 1 parseStack
 
     match currentOperator, previousOperator with
     | current, Some previous
         when precendence current <= precendence previous ->
-            rebuildTerm (terms, operators)
+            rebuildTerm parseStack
             |> handleOperator current
     | current, _ ->
-        terms, Stack.push current operators
+        Stack.push current parseStack
 
-let rec rebuildRemainingTerms (terms, operators) =
-    if operators |> Stack.isEmpty then
-        terms, operators
+let rec rebuildRemainingTerms (parseStack: Stack<string>) : Stack<string> =
+    if parseStack |> Stack.isEmpty || parseStack |> Stack.count = 1 then
+        parseStack
     else
-        rebuildTerm (terms, operators)
+        rebuildTerm parseStack
         |> rebuildRemainingTerms
 
 let sya (exprS: string) =
     let expr = exprS.Split(" ") |> Array.toList
 
-    let rec loop tokens (terms, operators) =
+    let rec loop tokens parseStack =
         match tokens with
         | [] ->
-            rebuildRemainingTerms (terms, operators)
+            rebuildRemainingTerms parseStack
         | token::tokens ->
             match token with
             | Operator (operator) ->
-                handleOperator operator (terms, operators)
+                handleOperator operator parseStack
                 |> loop tokens
             | Term (term) ->
-                loop tokens ((Stack.push term terms), operators)
+                loop tokens (Stack.push term parseStack)
 
-    loop expr (Stack.empty, Stack.empty)
+    loop expr Stack.empty
 
 sya "5 + 10 + 4 * 3 - 6 / 4"
 |> printfn "%A"
+
+Stack.empty
+|> Stack.push "5"
+|> Stack.push "+"
+|> Stack.push "10"
+|> rebuildTerm
